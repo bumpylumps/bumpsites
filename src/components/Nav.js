43647337a4c7e0379eb98526cf8ef6b0069a179d
bumpy_lumps <alexander.fulop.art@gmail.com>
2023-11-02 15:11:26 -0400

    fixed typo and updated Todo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <>
+    <section>
       <img src='logo' alt="headshot portrait" />
       <section>
         <a href=''>Projects</a>
@@ -10,7 +10,7 @@ const Nav = () => {
         <a href=''>Contact</a>
         <input type='button' value="Resume"/>
       </section>
-    </>
+    </section>
   )
 }
 
